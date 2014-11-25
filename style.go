b8ab721ac836c6bb09a27f3a6b81a659701fd708
Bryan Burke <bryan.t.burke@gmail.com>
2014-11-25 17:08:51 -0500

    Implement Style

--- style.go
+++ style.go
@@ -1,63 +1,110 @@
 package mcclintock
 
 import (
+	"bytes"
 	"fmt"
 )
 
 type color struct {
 	before int
-	after int
+	after  int
 }
 
-func (c color) Codes() (int, int) { return c.before, c.after }
+func (c color) codes() (int, int) { return c.before, c.after }
 
 type textstyle struct {
 	before int
-	after int
+	after  int
 }
 
-func (t textstyle) Codes() (int, int) { return t.before, t.after }
+func (t textstyle) codes() (int, int) { return t.before, t.after }
 
-type Style interface {
-	Codes() (int, int)
+type styleInterface interface {
+	codes() (int, int)
+}
+
+// style represents a computed style from one or more colors or textstyles
+// as the ANSI code suitable for terminal output
+type style struct {
+	before string
+	after  string
+}
+
+// ApplyTo applies styles created using the Style command to a string
+// to generate an styled output using ANSI terminal codes
+func (s *style) ApplyTo(content string) string {
+	var out bytes.Buffer
+	out.WriteString(s.before)
+	out.WriteString(content)
+	out.WriteString(s.after)
+	return out.String()
 }
 
 var (
 	// Colors
-	Black   = color{0, 9}
-	Red     = color{1, 9}
-	Green   = color{2, 9}
-	Yellow  = color{3, 9}
-	Blue    = color{4, 9}
-	Magenta = color{5, 9}
-	Cyan    = color{6, 9}
-	White   = color{7, 9}
-	Default = color{9, 9}
+	Black   = color{30, 39}
+	Red     = color{31, 39}
+	Green   = color{32, 39}
+	Yellow  = color{33, 39}
+	Blue    = color{34, 39}
+	Magenta = color{35, 39}
+	Cyan    = color{36, 39}
+	White   = color{37, 39}
+	Default = color{39, 39}
 
 	// Shortcut Colors
-	K = color{0, 9}
-	R = color{1, 9}
-	G = color{2, 9}
-	Y = color{3, 9}
-	B = color{4, 9}
-	M = color{5, 9}
-	C = color{6, 9}
-	W = color{7, 9}
-	D = color{9, 9}
+	K   = color{30, 39}
+	R   = color{31, 39}
+	G   = color{32, 39}
+	Y   = color{33, 39}
+	B   = color{34, 39}
+	M   = color{35, 39}
+	C   = color{36, 39}
+	W   = color{37, 39}
+	Def = color{39, 39}
 
 	// Textstyles
-	Bold          = textstyle{1, 22}
-	Italic        = textstyle{3, 23}
-	Underline     = textstyle{4, 24}	
-
+	Bold      = textstyle{1, 22}
+	Italic    = textstyle{3, 23}
+	Underline = textstyle{4, 24}
 )
 
-type text struct {}
-
-func Text() *text {
-	return &text{}
+// Background returns a style that sets the background to the appropriate color
+func Background(c *color) *color {
+	c.before += 10
+	c.after += 10
+	return c
 }
 
-func (t *text) Red(s string) string {
-	return fmt.Sprintf("\x1b[31m%s\x1b[39m")
+func Style(s ...styleInterface) *style {
+	switch {
+	case len(s) == 1:
+		bef, aft := s[0].codes()
+		var computedStyle style
+		computedStyle.before = fmt.Sprintf("\x1b[%vm", bef)
+		computedStyle.after = fmt.Sprintf("\x1b[%vm", aft)
+		return &computedStyle
+	case len(s) > 1:
+		var computedStyle style
+		var beforeConcat, afterConcat bytes.Buffer
+
+		beforeConcat.WriteString("\x1b[")
+		afterConcat.WriteString("\x1b[")
+
+		var bef, aft int
+		for idx, sty := range s {
+			bef, aft = sty.codes()
+			if idx < len(s)-1 {
+				beforeConcat.WriteString(fmt.Sprintf("%v; ", bef))
+				afterConcat.WriteString(fmt.Sprintf("%v; ", aft))
+			} else {
+				beforeConcat.WriteString(fmt.Sprintf("%vm", bef))
+				afterConcat.WriteString(fmt.Sprintf("%vm", aft))
+			}
+		}
+		computedStyle.before = beforeConcat.String()
+		computedStyle.after = afterConcat.String()
+		return &computedStyle
+	}
+	return &style{}
 }
---
+++ style_test.go
@@ -0,0 +1,41 @@
+package mcclintock
+
+import "testing"
+
+func TestStyle1(t *testing.T) {
+	s := Style(Red)
+	expectBefore := "\x1b[31m"
+	expectAfter := "\x1b[39m"
+	if s.before != expectBefore || s.after != expectAfter {
+		t.Errorf("Expected:\nBefore: %s After: %s\nGot:\nBefore: %s After:%s\n", expectBefore, expectAfter, s.before, s.after)
+	}
+}
+
+func TestStyle2(t *testing.T) {
+	s2 := Style(Red, Underline)
+	expectBefore2 := "\x1b[31; 4m"
+	expectAfter2 := "\x1b[39; 24m"
+	if s2.before != expectBefore2 || s2.after != expectAfter2 {
+		t.Errorf("Expected:\nBefore: %s After: %s\nGot:\nBefore: %s After: %s\n", expectBefore2, expectAfter2, s2.before, s2.after)
+	}
+}
+
+func TestApplyTo(t *testing.T) {
+	s := Style(Red)
+	testString := "This is a test"
+	expect := "\x1b[31mThis is a test\x1b[39m"
+	applyResult := s.ApplyTo(testString)
+	if applyResult != expect {
+		t.Errorf("Expected: %v\nGot: %v\n", expect, applyResult)
+	}
+}
+
+func TestApplyTo2(t *testing.T) {
+	s := Style(Red, Underline)
+	testString := "This is a test"
+	expect := "\x1b[31; 4mThis is a test\x1b[39; 24m"
+	applyResult := s.ApplyTo(testString)
+	if applyResult != expect {
+		t.Errorf("Expected: %v\nGot: %v\n", expect, applyResult)
+	}
+}
