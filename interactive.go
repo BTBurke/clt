12d413f482fecc411599f9a44b09ea9488a64681
Bryan Burke <bryan.t.burke@gmail.com>
2017-10-17 22:32:53 -0400

    refactor to simplify interfaces for interactive sessions

--- interactive.go
+++ interactive.go
@@ -3,6 +3,8 @@ package clt
 import (
 	"bufio"
 	"fmt"
+	"io"
+
 	"os"
 	"strings"
 
@@ -12,61 +14,71 @@ import (
 // InteractiveSession creates a system for collecting user input
 // in response to questions and choices
 type InteractiveSession struct {
-	input  *InputReader
-	output *OutputWriter
+	Prompt   string
+	Response string
+	Default  string
+	ValHint  string
+
+	input  *bufio.Reader
+	output io.Writer
 }
 
 // NewInteractiveSession returns a new InteractiveSession outputting to Stdout
 // and reading from Stdin
 func NewInteractiveSession() *InteractiveSession {
 	return &InteractiveSession{
-		input:  NewInputReader(),
-		output: NewOutputWriter(),
+		input:  bufio.NewReader(os.Stdin),
+		output: bufio.NewWriter(os.Stdout),
 	}
 }
 
-// SayAndThen creates a long-form prompt and then asks for some type of user input.  You can use this
-// to add information ahead of asking for user input.  It can be used with any terminator such as Render,
-// Pause, Ask, or Choice.
-func (i *InteractiveSession) SayAndThen(format string, args ...interface{}) *InteractiveSession {
-	i.output.Add(format, args...)
+// Say
+func (i *InteractiveSession) Say(format string, args ...interface{}) *InteractiveSession {
+	fmt.Fprintf(i.output, format, args...)
 	return i
-}
-
-// Say is a terminator that immediately renders information to the console and does not ask for
-// any type of user input.
-func (i *InteractiveSession) Say(format string, args ...interface{}) {
-	i.SayAndThen(format, args...).Render()
 }
 
 // Pause is a terminator that will render long-form text added via the another method
 // that returns *InteractiveSession and will wait for the user to press enter to continue.
 // It is useful for long-form content or paging.
 func (i *InteractiveSession) Pause() {
-	c := NewInputReader()
-	c.Prompt = i.output.Add("\n\nPress [Enter] to continue.\n\n").Finalize()
-	i.input.Ask()
+	i.Prompt = "\n\nPress [Enter] to continue.\n\n"
+	i.get()
+}
+
+func (i *InteractiveSession) get() (err error) {
+	switch {
+	case len(i.Default) > 0:
+		fmt.Fprintf(i.output, "%s  [%s]: ", i.Prompt, i.Default)
+	case len(i.ValHint) > 0:
+		fmt.Fprintf(i.output, "%s (%s): ", i.Prompt, i.ValHint)
+	default:
+		fmt.Fprintf(i.output, "%s: ", i.Prompt)
+	}
+
+	i.Response, err = i.input.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	if len(i.Default) > 0 && len(i.Response) == 0 {
+		i.Response = i.Default
+	}
+	i.Response = strings.TrimSpace(i.Response)
+	return nil
 }
 
 // Warn adds an informational warning message to the user in format
 // Warning: <user defined string>
 func (i *InteractiveSession) Warn(format string, args ...interface{}) *InteractiveSession {
-	i.output.Add("%s: %s", Style(Yellow).ApplyTo("Warning"), fmt.Sprintf(format, args...))
+	fmt.Fprintf(i.output, "%s: %s", Style(Yellow).ApplyTo("Warning"), fmt.Sprintf(format, args...))
 	return i
 }
 
 // Error is a terminator that gives an informational error message to the user in format
 // Error: <user defined string>.  Exits the program returning status code 1
 func (i *InteractiveSession) Error(format string, args ...interface{}) {
-	i.output.Add("%s: %s", Style(Red).ApplyTo("Error:"), fmt.Sprintf(format, args...)).Render()
+	fmt.Fprintf(i.output, "\n\n%s: %s\n", Style(Red).ApplyTo("Error:"), fmt.Sprintf(format, args...))
 	os.Exit(1)
-}
-
-// Render is a terminator for an interactive session that provides information to the
-// user but does not ask for any input.  To retrieve info, you should terminate
-// with Ask, Choice, or another terminator that returns user input.
-func (i *InteractiveSession) Render() {
-	i.output.Render()
 }
 
 // Ask is a terminator for an interactive session that results in returning the user
@@ -85,47 +97,45 @@ func (i *InteractiveSession) AskWithHint(prompt string, hint string, validators 
 }
 
 func (i *InteractiveSession) ask(prompt string, def string, hint string, validators ...ValidationFunc) string {
-	c := NewInputReader()
-	c.Prompt = prompt
-	i.input.Default = def
-	i.input.ValHint = hint
-	i.Render()
-	i.input.Ask()
+	i.Prompt = prompt
+	i.Default = def
+	i.ValHint = hint
+	i.get()
 	for _, validator := range validators {
-		if ok, err := validator(i.input.Response); !ok {
-			i.Say("Error: %s", err)
-			i.Ask(prompt, validators...)
+		if ok, err := validator(i.Response); !ok {
+			i.Say("\nError: %s\n\n", err)
+			i.ask(prompt, def, hint, validators...)
 		}
 	}
-	return i.input.Response
+	return i.Response
 }
 
 // AskPassword is a terminator that asks for a password and does not echo input
 // to the terminal.
 func (i *InteractiveSession) AskPassword(validators ...ValidationFunc) string {
-	rw := bufio.NewReadWriter(i.input.reader, bufio.NewWriter(i.output.writer))
+	rw := bufio.NewReadWriter(i.input, bufio.NewWriter(i.output))
 	term := terminal.NewTerminal(rw, "")
 	pw, err := term.ReadPassword("Password: ")
 	if err != nil {
-		i.Error("%s", err)
+		i.Error("\n%s\n\n", err)
 	}
 	for _, validator := range validators {
 		if ok, err := validator(strings.TrimSpace(pw)); !ok {
-			i.Say("Error: %s", err)
+			i.Say("\nError: %s\n\n", err)
 			i.AskPassword(validators...)
 		}
 	}
 	return strings.TrimSpace(pw)
 }
 
-func (i *InteractiveSession) AskYesNo(prompt string, defaultChoice string) string {
+func (i *InteractiveSession) YesNo(prompt string, defaultChoice string) string {
 	switch def := strings.ToLower(defaultChoice); def {
 	case "y", "yes":
-		i.input.Default = "Y/n"
+		i.Default = "Y/n"
 	default:
-		i.input.Default = "y/N"
+		i.Default = "y/N"
 	}
-	return i.ask(prompt, i.input.Default, "", ValidateYesNo())
+	return i.ask(prompt, i.Default, "", ValidateYesNo())
 }
 
 func (i *InteractiveSession) AskFromTable(prompt string, choices map[string]string, def string) string {
@@ -137,14 +147,13 @@ func (i *InteractiveSession) AskFromTable(prompt string, choices map[string]stri
 	}
 	tAsString := t.renderTableAsString()
 
-	i.input.Prompt = fmt.Sprintf("%s\n%s\nChoice [%s]: ", prompt, tAsString, choices[def])
-	i.input.Default = def
-	i.Render()
-	i.input.Ask()
-	if ok, err := AllowedOptions(allKeys)(i.input.Response); !ok {
-		i.Say("Error: %s", err)
+	i.Prompt = fmt.Sprintf("%s\n%s\n\nChoice [%s]: ", prompt, tAsString, choices[def])
+	i.Default = def
+	i.get()
+	if ok, err := AllowedOptions(allKeys)(i.Response); !ok {
+		i.Say("\nError: %s\n\n", err)
 		i.AskFromTable(prompt, choices, def)
 	}
 
-	return strings.TrimSpace(i.input.Response)
+	return strings.TrimSpace(i.Response)
 }
--- interactive_test.go
+++ interactive_test.go
@@ -1,1 +1,56 @@
 package clt
+
+import (
+	"bufio"
+	"bytes"
+	"testing"
+
+	"github.com/BTBurke/snapshot"
+)
+
+func WithInput(input string) (*InteractiveSession, *bytes.Buffer) {
+	var out bytes.Buffer
+	return &InteractiveSession{
+		input:  bufio.NewReader(bytes.NewBufferString(input)),
+		output: &out,
+	}, &out
+}
+
+func TestSay(t *testing.T) {
+
+	tt := []struct {
+		Name       string
+		Method     string
+		Prompt     string
+		Default    string
+		ValHint    string
+		Options    map[string]string
+		Input      string
+		Resp       string
+		Validators []ValidationFunc
+	}{
+		{Name: "simple ask", Method: "ask", Prompt: "Did this work", Input: "yes\n", Resp: "yes"},
+		{Name: "simple yn", Method: "yn", Prompt: "Do you want this to work", Input: "y", Default: "n", Resp: "y"},
+		{Name: "retry yn", Method: "yn", Prompt: "Do you want this to work", Input: "q\ny", Default: "n", Resp: "y"},
+	}
+	for _, tc := range tt {
+		t.Run(tc.Name, func(t *testing.T) {
+			sess, buf := WithInput(tc.Input)
+
+			var got string
+			switch tc.Method {
+			case "ask":
+				got = sess.ask(tc.Prompt, tc.Default, tc.ValHint, tc.Validators...)
+			case "yn":
+				got = sess.YesNo(tc.Prompt, tc.Default)
+			default:
+				t.Errorf("unexpected test: %s", tc.Method)
+			}
+			if tc.Resp != got {
+				t.Errorf("interactive session returned bad response, expected %s, got %s", tc.Resp, got)
+			}
+			snapshot.Assert(t, buf.Bytes())
+		})
+
+	}
+}
