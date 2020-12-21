3b539e8072340ebb5f2f554e4a14b5218bd7b972
Bryan Burke <bryan.t.burke@gmail.com>
2020-12-21 21:43:28 +0200

    fixes cross os builds for table size

--- table.go
+++ table.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"os"
 	"strings"
-	"syscall"
-	"unsafe"
 )
 
 // Justification sets the default placement of text inside each cell of a column
@@ -109,20 +107,6 @@ func Spacing(n int) TableOption {
 		}
 		return nil
 	}
-}
-
-// Magic from the go source for ssh/terminal to find terminal size.  Because it is
-// difficult to test this implementation across os/terminal combinations, you can skip
-// this check by setting Table.SkipTermSize=true and set the Table.maxWidth and
-// Table.MaxHeight manually.  Or, Table will set appropriate conservative defaults.
-func getTerminalSize() (width, height int, err error) {
-	fd := os.Stdout.Fd()
-	var dimensions [4]uint16
-
-	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), uintptr(syscall.TIOCGWINSZ), uintptr(unsafe.Pointer(&dimensions)), 0, 0, 0); err != 0 {
-		return -1, -1, err
-	}
-	return int(dimensions[1]), int(dimensions[0]), nil
 }
 
 func (r *Row) addCell(c Cell) {
---
+++ table_size_windows.go
@@ -0,0 +1,8 @@
+// +build windows
+package clt
+
+import "fmt"
+
+func getTerminalSize() (width, height int, err error) {
+	return -1, -1, fmt.Errorf("fuck windows")
+}
