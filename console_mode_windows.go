49ff6caeb0672ca5f42d386119721e3c9892bf7d
Bryan Burke <bryan.t.burke@gmail.com>
2020-12-22 11:21:37 +0200

    enables ANSI mode in windows 10 terminal

---
+++ console_mode_windows.go
@@ -0,0 +1,34 @@
+// +build windows
+
+package clt
+
+import (
+	"log"
+	"os"
+	"syscall"
+)
+
+func init() {
+	enableANSI()
+}
+
+var (
+	dll            = syscall.MustLoadDLL("kernel32")
+	setConsoleMode = dll.MustFindProc("SetConsoleMode")
+)
+
+func setInputConsoleMode(h syscall.Handle, m uint32) error {
+	r, _, err := setConsoleMode.Call(uintptr(h), uintptr(m))
+	if r == 0 {
+		return err
+	}
+	return nil
+}
+
+func enableANSI() {
+	log.Printf("enabled ANSI handling")
+	h := syscall.Handle(os.Stdin.Fd())
+	if err := setInputConsoleMode(h, 0); err != nil {
+		log.Printf("error setting ANSI handling: %s", err)
+	}
+}
